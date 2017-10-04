db9268fd240ec4e6a0bd578d1a56697abb1339eb
Benjamin de Bos <benjamindebos@usenetbucket.com>
2017-10-04 16:34:50 +0200

    POD headers

--- Pods/Headers/Private/MSSimpleGauge/MSGradientArcLayer.h
+++ Pods/Headers/Private/MSSimpleGauge/MSGradientArcLayer.h
@@ -1,1 +1,15 @@
-../../../MSSimpleGauge/SimpleGauge/Gauges/MSGradientArcLayer.h
+//
+//  PBGradientArcLayer.h
+//  SimpleGauge
+//
+//  Created by Mike Sabatini on 1/15/13.
+//  Copyright (c) 2013 Mike Sabatini. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+#import "MSArcLayerSubclass.h"
+
+@interface MSGradientArcLayer : MSArcLayer
++ (CGGradientRef)defaultGradient;
+@property (nonatomic,assign) CGGradientRef gradient;
+@end
--- Pods/Headers/Private/QrcodeBuilder/QrcodeBuilder.h
+++ Pods/Headers/Private/QrcodeBuilder/QrcodeBuilder.h
@@ -1,1 +1,14 @@
-../../../QrcodeBuilder/QrcodeBuilder/Classes/QrcodeBuilder.h
+//
+//  QrcodeBuilder.h
+//  Pods
+//
+//  Created by Corotata on 2017/7/13.
+//
+//
+
+#ifndef QrcodeBuilder_h
+#define QrcodeBuilder_h
+
+#import "UIImage+Coro_QrcodeBuilder.h"
+
+#endif /* QrcodeBuilder_h */
